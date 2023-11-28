761224fe0ac0776d29277cd4f5b24fc4e11a2277
Ahnue999 <ahnue200572@gmail.con>
2023-11-28 12:08:02 +0300

    inset

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,5 +2,43 @@
 
 listint_t *insert_node(listint_t **head, int number)
 {
+	listint_t *curr, *after, *new;
 
+	new = malloc(sizeof(listint_t));
+
+	new->n = number;
+	new->next = NULL;
+
+	if (!*head)
+	{
+		*head = new;
+		return (*head)
+	}
+
+	curr = *head;
+	after = (*head)->next
+	while (after)
+	{
+		if (new->n <= curr->n && new->n <= after->n)
+		{
+			curr->next = new;
+			new->next = after;
+			return (new);
+		}
+		curr = after;
+		after = after->next;
+	}
+
+	if (new->n >= curr->n)
+	{
+		curr->next = new;
+		return (new);
+	}
+	else
+	{
+		new->next = curr;
+		curr->next = NULL;
+		*head = new;
+		return (new);
+	}
 }
