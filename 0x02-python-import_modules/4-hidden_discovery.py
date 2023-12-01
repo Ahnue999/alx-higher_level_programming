b7cca98947f9e66616987729d6c440e686693c4e
Ahnue999 <ahnue200572@gmail.con>
2023-12-01 05:23:20 +0300

    commit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
+
     import hidden_4
+
     for file in dir(hidden_4):
-        if file.startswith("__"):
-            continue
-        else:
+        if file[0] != "_" and file[1] != "_":
             print(file)
