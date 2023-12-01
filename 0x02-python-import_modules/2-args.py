3ce61a148df3a2947629feeda46d7105213dd473
Ahnue999 <ahnue200572@gmail.com>
2023-11-30 17:57:00 -0800

    committed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,16 +2,16 @@
 
 import sys
 
-length = len(sys.argv)
+args = len(sys.argv) - 1
 
-if length == 1:
-    print("{:d} argument".format(length), end="")
-    print("{:d}: {:s}".format(1, sys.argv[0]))
+if args == 1:
+    print("{:d} argument".format(args), end="")
+    print("{:d}: {:s}".format(args, sys.argv[args]))
 else:
-    print("{:d} arguments".format(length), end="")
-    if length == 0:
+    print("{:d} arguments".format(args), end="")
+    if args == 0:
         print(".")
     else:
         print(":")
-        for i in range(length):
-            print("{:d}: {:s}".format(i + 1, sys.argv[i]))
+        for i in range(1, args + 1):
+            print("{:d}: {:s}".format(i, sys.argv[i]))
