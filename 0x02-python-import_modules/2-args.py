c45b5c6c19fad75ba4456549524ff8e3308607cc
Ahnue999 <ahnue200572@gmail.con>
2023-12-01 04:47:55 +0300

    c

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    
+    import sys
+
+    length = len(sys.argv)
+
+    if length == 1:
+        print("{:d} argument".format(length), end="")
+        print("{:d}: {:s}".format(1, sys.argv[0]))
+    else:
+        print("{:d} arguments".format(length), end="")
+        if length == 0:
+            print(".")
+        else:
+            print(":")
+            for i in range(length):
+                print("{:d}: {:s}".format(i + 1, sys.argv[i]))
