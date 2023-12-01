a5469ec160d074c5a3f194da6fa635c8ce6628f6
Ahnue999 <ahnue200572@gmail.con>
2023-12-01 05:04:23 +0300

    commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
         else:
             print(":")
             for i in range(1, args + 1):
-               print("{:d}: {:s}".format(i, sys.argv[i]))
+                print("{:d}: {:s}".format(i, sys.argv[i]))
