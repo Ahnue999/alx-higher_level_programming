b6051c7a36cafa6587aca811e9e25717520f52a2
Ahnue999 <ahnue200572@gmail.con>
2023-12-02 06:40:56 +0300

    commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if 
     length = len(my_list)
-    for i in range(length):
-        print("{:d}".format(my_list[i]))
+    if (idx > length - 1 or idx < 0):
+        return None
+    return my_list[idx]
