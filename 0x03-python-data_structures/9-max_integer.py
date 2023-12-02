aece7896cafacefc19b9ddf5e96c668a72011eb8
Ahnue999 <ahnue200572@gmail.con>
2023-12-02 10:38:22 +0300

    commit

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length <= 0:
+        return None
+    max = my_list[0]
+    for i in range(length):
+        if my_list[i] > max:
+            max = my_list[i]
+    return max
