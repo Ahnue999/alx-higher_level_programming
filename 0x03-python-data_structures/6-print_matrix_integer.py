2677b9384a80106de21f91dc2805e904ac313700
Ahnue999 <ahnue200572@gmail.con>
2023-12-03 19:12:01 +0300

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,6 +6,6 @@ def print_matrix_integer(matrix=[[]]):
     for i in range(rows):
         for j in range(cols):
             if j != 0:
-                print("",end=" ")
+                print("", end=" ")
             print("{:d}".format(matrix[i][j]), end="")
         print()
