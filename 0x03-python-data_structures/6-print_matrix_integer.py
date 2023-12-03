dc2c440a0f31f2807bbf2cf196f9e93e92671f0d
Ahnue999 <ahnue200572@gmail.con>
2023-12-03 18:52:15 +0300

    commit

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    rows = len(matrix)
-    if rows <= 0:
-        return
-    cols = len(matrix[0])
-    for i in range(rows):
-        for j in range(cols):
-            if j == cols - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), end=" ")
+    if not my_lsit:
+        pass
+    else:
+        rows = len(matrix)
+        if rows <= 0:
+            return
+        cols = len(matrix[0])
+        for i in range(rows):
+            for j in range(cols):
+                if j == cols - 1:
+                    print("{:d}".format(matrix[i][j]))
+                else:
+                    print("{:d}".format(matrix[i][j]), end=" ")
