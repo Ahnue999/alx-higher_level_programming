c0abdd7feecb246bd64363e20740418161d90242
Ahnue999 <ahnue200572@gmail.com>
2023-12-03 07:41:15 -0800

    committed

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 
 def delete_at(my_list=[], idx=0):
     length = len(my_list)
-    if length <= 0 or idx > length - 1:
+    if length <= 0:
         return None
     new = []
     for i in range(length):
