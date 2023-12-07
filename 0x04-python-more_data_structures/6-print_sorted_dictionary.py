514a952adb1f3c8d2a18fa9a128f0574498ecefe
Ahnue999 <ahnue200572@gmail.con>
2023-12-07 03:02:37 +0300

    commit

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    sorted = a_dictionary.keys()
-    list(map(lambda key: print(a_dictionary[key]), sorted))
+    sorted = list(a_dictionary.keys())
+    sorted.sort()
+    list(map(lambda key: print(key, ": ", a_dictionary[key], sep=""), sorted))
