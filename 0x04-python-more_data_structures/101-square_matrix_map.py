e417fdb1a8584e6caf38ec85c0724aef734ef769
Ahnue999 <ahnue200572@gmail.con>
2023-12-07 03:51:56 +0300

    commit

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-
-def square_matrix_simple(matrix=[]):
-    rows = len(matrix)
-    new = matrix.copy()
-    for i in range(rows):
-        new[i] = list(map(lambda x: x ** 2, matrix[i]))
-
-    return (new)
+def square_matrix_map(matrix=[]):
+        return list(map(lambda x: x ** 2, matrix))
