55efdaac06e5a6ff648a10f4a2fe4700ba5b2d6d
Ahnue999 <ahnue200572@gmail.con>
2023-12-07 04:17:32 +0300

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -9,7 +9,6 @@ def to_sub(list_num):
 
     return (max_list - to_sub)
 
-
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
@@ -17,7 +16,8 @@ def roman_to_int(roman_string):
     if not isinstance(roman_string, str):
         return 0
 
-    rom_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom_nums = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                'C': 100, 'D': 500, 'M': 1000}
     list_keys = list(rom_nums.keys())
 
     num = 0
