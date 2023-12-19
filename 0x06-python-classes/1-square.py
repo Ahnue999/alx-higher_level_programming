628cc549eb1c4fc81dbeba8566850242faaf019d
Ahnue999 <ahnue200572@gmail.com>
2023-12-19 11:29:22 -0800

    committed

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"A module for a class that defines a square"
+
+
+class Square:
+    "a square class"
+    def __init__(self, size):
+	    "initialaize a square"
+	    self.__size = size
+	    
