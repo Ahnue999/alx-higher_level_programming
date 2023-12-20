ecd4dbee16613f7dd33c13d503d51acd9229197a
Ahnue999 <ahnue200572@gmail.con>
2023-12-20 21:11:17 +0300

    commit

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,31 +3,37 @@
 
 class Node:
     def __init__(self, data, next_node=None):
+        """initialaizing the class"""
         self.__data = data
         self.__next_node = next_node
 
     @property
     def data(self):
+        """data getter"""
         return (self.__data)
 
     @data.setter
     def data(self, value):
+        """data setter"""
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
+        """next_node getter"""
         return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
+        """next_node setter"""
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 class SinglyLinkedList:
     def __init__(self):
+        """initialaizing the class"""
         self.__head = None
 
     def __str__(self):
