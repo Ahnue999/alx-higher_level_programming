89394f5e99d7a6209e11afde260c56367410556a
Ahnue999 <ahnue200572@gmail.con>
2023-12-20 18:24:22 +0300

    commit

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,14 +6,20 @@ class Square:
     "a square class"
     def __init__(self, size=0):
         "initialaize a square"
-        if type(size) == int:
-            if size >= 0:
-                self.__size = size
+    def area(self):
+        """A method that returns the are of a square"""
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) == int:
+            if value >= 0:
+                self.__size = value
             else:
                 raise ValueError("size must be >= 0")
         else:
             raise TypeError("size must be an integer")
-
-    def area(self):
-        """A method that returns the are of a square"""
-        return (self.__size ** 2)
