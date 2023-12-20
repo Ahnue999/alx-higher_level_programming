9898207c77075e6894edfc39bbb7d3bc6fc99cd6
Ahnue999 <ahnue200572@gmail.con>
2023-12-20 18:13:42 +0300

    commit

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"A module for a class that defines a square"
+
+
+class Square:
+    "a square class"
+    def __init__(self, size=0):
+        "initialaize a square"
+        if type(size) == int:
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
