ddef89cdaff2446dcc9ea5b20f81ea641473a23b
Ahnue999 <ahnue200572@gmail.con>
2024-01-06 07:54:57 +0300

    commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,41 @@
 #!/usr/bin/python3
+"""A simple rectangle"""
+
+
+class Rectangle:
+    """Rectangle"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width
+        """
+        return slef.__width
+
+    @property
+    def height(self):
+        """height
+        """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """width setter.
+        """
+        if (not isinstance(value, int)):
+            raise TypeError("width must be an integer")
+        if (value < 0):
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """height setter.
+        """
+        if (not isinstance(value, int)):
+            raise TypeError("height must be an integer")
+        if (value < 0):
+            raise ValueError("height must be >= 0")
+        self.__height = value
