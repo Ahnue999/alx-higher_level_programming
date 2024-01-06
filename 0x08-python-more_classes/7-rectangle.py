3b7aa716818779d23e147df6684c9176276dd357
Ahnue999 <ahnue200572@gmail.con>
2024-01-06 10:12:50 +0300

    commit

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -64,7 +64,7 @@ class Rectangle:
             return ""
 
         for i in range(self.__height):
-            [print(type(self).print_symbol, end="") for j in range(self.__width)]
+            [print(self.print_symbol, end="") for j in range(self.__width)]
             if i < self.__height - 1:
                 print()
         return ("")
