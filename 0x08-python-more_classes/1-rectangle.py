c07c322aa35d4f06126719ab9a7e20a4020f3919
Ahnue999 <ahnue200572@gmail.con>
2024-01-06 08:35:42 +0300

    commit

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A simple rectangle"""
+"""A simple rectangle
+"""
 
 
 class Rectangle:
@@ -25,9 +26,9 @@ class Rectangle:
     def width(self, value):
         """width setter.
         """
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -35,8 +36,8 @@ class Rectangle:
     def height(self, value):
         """height setter.
         """
-        if (type(value) is not int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
