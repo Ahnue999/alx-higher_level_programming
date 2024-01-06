42cd1cb8439e386ba482cdb5480e45d449e40a66
Ahnue999 <ahnue200572@gmail.con>
2024-01-06 08:07:48 +0300

    commit

--- 0x08-python-more_classes/1-rec.py
+++ 0x08-python-more_classes/1-rec.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-""" empty class Rectangle that defines a rectangle
-"""
+"""A simple rectangle"""
 
 
 class Rectangle:
-    """ class rectangle"""
+    """Rectangle"""
     def __init__(self, width=0, height=0):
-        """ Instantiation with optional width and height"""
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
-        """ width
+        """width
         """
-        return self.__width
+        return slef.__width
 
     @property
     def height(self):
@@ -24,20 +22,20 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """ width setter
+        """width setter.
         """
-        if type(value) is not int:
+        if (not isinstance(value, int)):
             raise TypeError("width must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @height.setter
     def height(self, value):
-        """ height setter
+        """height setter.
         """
-        if type(value) is not int:
+        if (not isinstance(value, int)):
             raise TypeError("height must be an integer")
-        if value < 0:
+        if (value < 0):
             raise ValueError("height must be >= 0")
         self.__height = value
