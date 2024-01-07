e5aeb40ab02143fb908a479bda2f95f57b67dca7
Ahnue999 <ahnue200572@gmail.con>
2024-01-08 01:38:36 +0300

    commit

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3.5
 """Multiply two matrices
 """
-import numpy as nump
+import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """Multiply two matrices
     """
 
-
-    return (nump.matmul(m_a, m_b))
+    return (np.matmul(m_a, m_b))
