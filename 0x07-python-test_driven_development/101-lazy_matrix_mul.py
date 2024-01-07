4a99b0f246760525706d2739e1d52115bf32ccd6
Ahnue999 <ahnue200572@gmail.com>
2024-01-07 14:16:10 -0800

    committed

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3.5
+"""
+
+Module composed by a function that multiplies 2 matrices
+
+"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+
+
+    return (np.matmul(m_a, m_b))
