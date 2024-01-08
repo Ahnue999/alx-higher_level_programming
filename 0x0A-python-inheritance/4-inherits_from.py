2d72c6adcf4c61de67f56f704a7ce02db4aff81a
Ahnue999 <ahnue200572@gmail.con>
2024-01-08 18:14:03 +0300

    commit

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,7 +5,7 @@
 def inherits_from(obj, a_class):
     """ returns true if an obj is a subclass of given class """
 
-    if type(obj) != a_class:
+    if type(obj) is not a_class:
         return issubclass(type(obj), a_class)
     else:
         return False
--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,7 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """ validates value """
 
-        if type(value) != int:
+        if type(value) is int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
