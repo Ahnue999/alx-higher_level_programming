c1aca96735339bc1b592db5a7e9d159ba121f315
Ahnue999 <ahnue200572@gmail.con>
2024-01-08 10:42:58 +0300

    commit

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" My int """
+
+class MyInt(int):
+    """ MyInt class """
+
+    def __eq__(self, other):
+        """ equality """
+        return int(self) != other
+
+    def __ne__(self, other):
+        """ equality """
+        return int(self) == other
