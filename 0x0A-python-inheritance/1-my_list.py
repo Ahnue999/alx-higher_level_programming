eae3687891f89652ba25f9472d757f7b03770f75
Ahnue999 <ahnue200572@gmail.con>
2024-01-08 09:23:42 +0300

    commit

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+""" A class that inherits from the list class """
+
+
+class MyList(list):
+    """ inherits from list"""
+
+    def print_sorted(self):
+        """ prints the sorted list """
+        print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" Checks if an obj is an instance of a specified class """
+
+
+def is_same_class(obj, a_class):
+    """ Checks an obj """
+
+    if type(obj) is a_class:
+        return True
+    return False
+
