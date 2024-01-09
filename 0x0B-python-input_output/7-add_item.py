503c572ed248f56a8504a672439bcf210ea802bb
Ahnue999 <ahnue200572@gmail.con>
2024-01-09 20:25:18 +0300

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,12 +1,20 @@
 #!/usr/bin/python3
 """ load add save """
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 import sys
 
 
-args = sys.argv
-filename = "add_item.json"
+if __name__ = __main__:
 
-save_to_json_file(args, filename)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    filename = "add_item.json"
+
+    try:
+        data = load_from_json_file(filename)
+    except FileNotFoundError:
+        data = []
+
+    data.extends(sys.args[1:])
+    save_to_json_file(data, filename)
