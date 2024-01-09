3dffd2a263727fc1426621e3063edb91b7c77b70
Ahnue999 <ahnue200572@gmail.con>
2024-01-09 20:48:27 +0300

    commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+""" a student """
+
+
+class Student:
+    """ student class """
+
+    def __init__(self, first_name, last_name, age):
+        """ init """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """ dictionary representaion """
+
+        data = {}
+
+        if hasattr(self, "__dict__"):
+            data = self.__dict__.copy()
+
+        return data
