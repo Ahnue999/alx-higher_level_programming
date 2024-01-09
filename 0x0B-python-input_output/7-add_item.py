4eccbc101f4f8423fcdc340907288b3356ef5595
Ahnue999 <ahnue200572@gmail.con>
2024-01-09 20:39:02 +0300

    commit

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 import sys
 
 
-if __name__ = __main__:
+if __name__ == "__main__":
 
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
     load_from_json_file = \
@@ -16,5 +16,5 @@ if __name__ = __main__:
     except FileNotFoundError:
         data = []
 
-    data.extends(sys.args[1:])
+    data.extend(sys.argv[1:])
     save_to_json_file(data, filename)
