03c85f59024658895d262023b2863bf6bc73881e
Ahnue999 <ahnue200572@gmail.con>
2024-01-09 22:02:38 +0300

    commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" appendd after """
+
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """ appendd after """
+
+    with open(filename, "r", encoding="utf-8") as fp:
+
+        lines = fp.readlines()
+
+        new = []
+        for line in lines:
+            new.append(line)
+            if search_string in line:
+                new.append(new_string)
+
+    with open(filename, "w", encoding='utf-8') as fp:
+
+        fp.writelines(new)
--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,14 +9,11 @@ def pascal_triangle(n):
         return []
 
     tri = [[1]]
-
     prev = [0, 1, 0]
-
     for i in range(n - 1):
         new = []
         for d in range(len(prev) - 1):
             new.append(prev[d] + prev[d + 1])
         tri.append(new)
         prev = [0] + new + [0]
-
     return (tri)
