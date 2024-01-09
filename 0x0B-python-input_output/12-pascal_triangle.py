e3e3d6df8e8472c5f5404cb813c3aa14c677205c
Ahnue999 <ahnue200572@gmail.con>
2024-01-09 21:44:51 +0300

    commit

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+""" returns a pascal triangle """
+
+
+def pascal_triangle(n):
+    """ returns a pascal triangle """
+
+
+    if n <= 0:
+        return []
+
+    tri = [[1]]
+
+    prev = [0, 1, 0]
+
+    for i in range(n - 1):
+        new = []
+        for d in range(len(prev) - 1):
+            new.append(prev[d] + prev[d + 1])
+        tri.append(new)
+        prev = [0] + new + [0]
+
+    return (tri)
