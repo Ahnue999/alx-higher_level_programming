9d198c3d603b9be89446e4bdc0eca9ce2241f02f
Ahnue999 <ahnue200572@gmail.con>
2024-01-09 20:44:28 +0300

    commit

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" class to json """
+
+
+def class_to_json(obj):
+    """ class to json """
+
+    data = {}
+
+    if hasattr(obj, "__dict__"):
+        data = obj.__dict__.copy()
+
+    return data
