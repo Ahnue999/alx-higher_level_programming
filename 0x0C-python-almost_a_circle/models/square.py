c5692c43abe751cd468eac0e673121e831d0b3c9
Ahnue999 <ahnue200572@gmail.con>
2024-01-16 00:32:15 +0300

    commit

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,44 +9,56 @@ class Square(Rectangle):
     """ a square class """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """ init """
+        """ init func """
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ str """
+        """ str func """
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
 
     @property
     def size(self):
-        """ size """
+        """ size getter """
         return self.width
 
     @size.setter
     def size(self, value):
-        """ size """
+        """ size setter"""
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
         """ update func """
-        if args != ():
-            attrs = ["id", "width", "x", "y"]
-            for i in range(len(args)):
-                if attrs[i] == "id":
-                    self.__dict__[attrs[i]] = args[i]
-                else:
-                    self.__dict__["_Rectangle__" + attrs[i]] = args[i]
-                if attrs[i] == "width":
-                    self.__dict__["_Rectangle__height"] = args[i]
-        elif kwargs is not None:
+
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.size = arg
+                elif a == 2:
+                    self.x = arg
+                elif a == 3:
+                    self.y = arg
+                a += 1
+
+        elif kwargs and len(kwargs) != 0:
             for key, value in kwargs.items():
-                if key != "id":
-                    key = "_Rectangle__" + key
-                if key == "_Rectangle__size":
-                    self.__dict__["_Rectangle__width"] = value
-                    self.__dict__["_Rectangle__height"] = value
-                else:
-                    self.__dict__[key] = value
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
 
     def to_dictionary(self):
         """ to dict """
