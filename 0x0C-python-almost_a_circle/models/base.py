e5c53196930aa75e6010e3934b5b82dd1b1cd999
Ahnue999 <ahnue200572@gmail.con>
2024-01-16 00:50:02 +0300

    commit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -143,23 +143,10 @@ class Base:
             turt.up()
             turt.goto(rect.x, rect.y)
             turt.down()
-            for i in range(2):
+            for j in range(4):
                 turt.forward(rect.width)
                 turt.left(90)
                 turt.forward(rect.height)
-                turt.left(90)
-            turt.hideturtle()
-
-        turt.color("#b5e3d8")
-        for sq in list_squares:
-            turt.showturtle()
-            turt.up()
-            turt.goto(sq.x, sq.y)
-            turt.down()
-            for i in range(2):
-                turt.forward(sq.width)
-                turt.left(90)
-                turt.forward(sq.height)
                 turt.left(90)
             turt.hideturtle()
 
