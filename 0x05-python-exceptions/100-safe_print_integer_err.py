6b16abeaa5b7aab90a1db053aa31c46f8a07dff7
Ahnue999 <ahnue200572@gmail.con>
2024-05-03 12:26:14 +0300

    commit

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#/usr/bin/python3
+
+def safe_print_integer_err(value):
+    """
+    safe print integer with error massage
+    """
+
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except ValueError as msg:
+        print("Exception:", msg)
+        return (False)
--- 0x05-python-exceptions/6-raise_exception_msg.py
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -7,8 +7,3 @@ def raise_exception_msg(message=""):
     """
 
     raise NameError(message)
-#    try:
-#        raise NameError
-#    except NameError:
-#        print(message)
-#        raise
