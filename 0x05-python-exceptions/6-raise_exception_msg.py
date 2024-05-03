64f557cf0ebee876918ec35cc8aadbe2c86090c9
Ahnue999 <ahnue200572@gmail.con>
2024-05-03 12:18:00 +0300

    commit

---
+++ 0x05-python-exceptions/6-raise_exception_msg.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def raise_exception_msg(message=""):
+    """
+    raise an exception with a msg
+    """
+
+    raise NameError(message)
+#    try:
+#        raise NameError
+#    except NameError:
+#        print(message)
+#        raise
