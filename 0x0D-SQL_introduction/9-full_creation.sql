74e03d89c90a339c3437b8eb06ab3858ed780f26
Ahnue999 <ahnue200572@gmail.com>
2024-07-09 16:49:01 +0300

    commit

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,11 @@
 -- Creates a second table
-CREATE TABLE IF NOT EXISTS second_table ( id INT,
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
 	name VARCHAR(256),
 	score INT
 );
 
+-- Inserting into the table.
 INSERT INTO second_table
 	(id, name, score)
 VALUES
