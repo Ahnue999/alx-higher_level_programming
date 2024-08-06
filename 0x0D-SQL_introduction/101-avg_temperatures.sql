7524b7d8ab9372de36f8e1622997728e2a38f92e
Ahnue999 <ahnue200572@gmail.com>
2024-08-06 07:46:48 +0300

    commit

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Desplay the avarage temperature by city ordered by temperature.
 USE hbtn_0c_0
-SELECT `city`, AVG(`value`) as `avg_temp`
+SELECT `city`, AVG(`value`) as `avg_temp` FROM temperatures
 	GROUP BY `city`
 	ORDER BY `avg_temp` DESC
