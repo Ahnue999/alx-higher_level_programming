2ff290983dce4439c34c92605de41e7c1553d451
Ahnue999 <ahnue200572@gmail.com>
2024-08-06 06:53:02 +0300

    commit

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- lists all records, skipping those without a name;
 SELECT `score`, `name` FROM second
-	WHERE `name` IS NOT NULL
-	ORDER BY `score` DESC;
+	WHERE `name` IS NOT ""
+	ORDER BY `score` DESC
