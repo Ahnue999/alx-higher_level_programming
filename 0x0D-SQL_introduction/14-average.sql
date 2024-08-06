44348e390730ef3b2c6cf6e1bf44aa5f4ebee9f4
Ahnue999 <ahnue200572@gmail.com>
2024-08-06 06:14:37 +0300

    commit

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Computes the score average of all records.
--- SELECT SUM(`score`)/COUNT(`name`) AS average FROM `second_table`
+-- SELECT SUM(`score`)/COUNT(*) AS average FROM `second_table`
 SELECT AVG(`score`) AS `average` FROM second_table;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- Lists the number of records with the same score.
+SELECT `score`, COUNT(*) FROM `second_table`
+	GROUP BY `score`;
