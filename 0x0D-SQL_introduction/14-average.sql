1a38a04a25aa05c76051aaaaf36554d2f0c4b551
Ahnue999 <ahnue200572@gmail.com>
2024-08-06 05:59:41 +0300

    commit

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Computes the score average of all records.
 -- SELECT SUM(`score`)/COUNT(`name`) AS average FROM `second_table`
-SELECT AVG(`score`) FROM second_table;
+SELECT AVG(`score`) AS `avarage` FROM second_table;
