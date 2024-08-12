3d2607c2ad51b4e25604e167e68b54cfe532b216
Ahnue999 <ahnue200572@gmail.com>
2024-08-12 11:17:25 +0300

    commit

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- sum of ratings for each show
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS `rating sum`
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS `rating`
 FROM tv_shows
 INNER JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
 GROUP BY tv_shows.title
-ORDER BY `rating sum` DESC
+ORDER BY `rating` DESC
